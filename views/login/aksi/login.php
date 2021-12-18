76994cec6327ce63b1fdf0a209f36872d16677ff
khaironbiz <khaironbiz@gmail.com>
2021-12-19 06:28:55 +0700

    logic login

--- views/login/aksi/login.php
+++ views/login/aksi/login.php
@@ -5,32 +5,35 @@ if(isset($_SESSION['login_user'])){
 }
 
 if (isset($_POST['key'])) {
+    $username   = $_POST['username'];
+    $password   = md5($_POST['password']);
+    $status     = 1;
+    $queryuser  = mysqli_query($host,"SELECT * FROM users WHERE email='$username' AND status='$status'");
+    $cont_email = mysqli_num_rows($queryuser);
+    //
+    $query      = mysqli_query($host,"SELECT * FROM users WHERE pass='$password' AND email='$username'");
+    $count_user = mysqli_num_rows($query);
+    $data_user  = mysqli_fetch_array($query);
     if (empty($_POST['username']) || empty($_POST['password'])) {
         $_SESSION['status']         = "Username or Password is required";
         $_SESSION['status_info']    = "danger";
         header("location: $site_url/login/");
         // Variabel username dan password
         }else{
-            $username   = $_POST['username'];
-            $password   = md5($_POST['password']);
-            $status     = 1;
-            $queryuser  = mysqli_query($host,"SELECT * FROM users WHERE email='$username' AND status='$status'");
-            $rows1      = mysqli_num_rows($queryuser);
-            if ($rows1 <1) {
+            if ($cont_email <1) {
                 $_SESSION['status']="Email tidak terdaftar";
                 $_SESSION['status_info']="danger";
                 echo "<script>document.location=\"$site_url/login/\"</script>";
             }else{
-                $query      = mysqli_query($host,"SELECT * FROM users WHERE pass='$password' AND email='$username'");
-                $rows       = mysqli_num_rows($query);
-                $data_user  = mysqli_fetch_array($query);
-                if ($rows == 1) {
+                if ($count_user == 1) {
                     $_SESSION['login_user'] = $data_user['id_user']; // Membuat Sesi/session
                     $_SESSION['email']      = $data_user['email']; // Membuat Sesi/session
                     $_SESSION['kel']        = $data_user['kel'];
+                    $_SESSION['status']="Login sukses";
+                    $_SESSION['status_info']="success";
                     header("location: $site_url/profile/"); // Mengarahkan ke halaman profil
                     }else{
-                        $_SESSION['status']="Maaf kombinasi username dan pasword tidak sesuai";
+                        $_SESSION['status']="Username & pasword not mach";
                         $_SESSION['status_info']="danger";
                         echo "<script>document.location=\"$site_url/login/\"</script>";
                     }
