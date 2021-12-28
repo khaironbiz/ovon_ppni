c5e36fe0625ff27188619dc03aee855d75f57582
khaironbiz <khaironbiz@gmail.com>
2021-12-28 09:48:24 +0700

    validasi aktivator

--- login/aktifasi.php
+++ login/aktifasi.php
@@ -2,8 +2,10 @@
 include("../auth/koneksi.php");
 if(isset($_GET['key'])){
     $key        = $_GET['key'];
-    $aktif_user = mysqli_query($host,"UPDATE users SET status = '1' WHERE has_user='$key'");
-    if($aktif_user){
+    $sql_key    = mysqli_query($host,"SELECT * FROM users WHERE has_user='$key'");
+    $count_key  = mysqli_num_rows($sql_key);
+    if($count_key >0 ){
+        $aktif_user = mysqli_query($host,"UPDATE users SET status = '1' WHERE has_user='$key'");
         $_SESSION['status']="Selamat akun anda telah aktif";
         $_SESSION['status_info']="success";
         echo "<script> alert(\"Selamat akun anda telah aktif\")</script>";
