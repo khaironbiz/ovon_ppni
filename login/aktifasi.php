759239cea37cde17f49a15443bcfcf9062c15ca1
khaironbiz <khaironbiz@gmail.com>
2021-12-28 09:51:53 +0700

    reset has user

--- login/aktifasi.php
+++ login/aktifasi.php
@@ -1,11 +1,14 @@
 <?php
 include("../auth/koneksi.php");
 if(isset($_GET['key'])){
-    $key        = $_GET['key'];
-    $sql_key    = mysqli_query($host,"SELECT * FROM users WHERE has_user='$key'");
-    $count_key  = mysqli_num_rows($sql_key);
+    $key            = $_GET['key'];
+    $sql_key        = mysqli_query($host,"SELECT * FROM users WHERE has_user='$key'");
+    $count_key      = mysqli_num_rows($sql_key);
+    $has_user_baru  = md5(uniqid());
     if($count_key >0 ){
-        $aktif_user = mysqli_query($host,"UPDATE users SET status = '1' WHERE has_user='$key'");
+        $aktif_user = mysqli_query($host,"UPDATE users SET 
+                            status      = '1',
+                            has_user    = '$has_user_baru' WHERE has_user='$key'");
         $_SESSION['status']="Selamat akun anda telah aktif";
         $_SESSION['status_info']="success";
         echo "<script> alert(\"Selamat akun anda telah aktif\")</script>";
