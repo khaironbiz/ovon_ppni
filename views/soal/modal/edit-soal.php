18a6d099b3fe81309dc08a236904619dbc2c815f
khaironbiz <khaironbiz@gmail.com>
2021-12-29 15:44:57 +0700

    edit modal soal

--- views/soal/modal/edit-soal.php
+++ views/soal/modal/edit-soal.php
@@ -1,10 +1,10 @@
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-dark btn-sm" data-toggle="modal" data-target="#exampleModal">
+<button type="button" class="btn btn-dark btn-sm" data-toggle="modal" data-target="#editsoal">
     Edit Pertanyaan
 </button>
 <!-- Modal -->
 <form action="" method="POST">
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="editsoal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
