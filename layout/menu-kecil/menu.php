4f230b250867643a48cd6d18dc4434c86be6866c
khaironbiz <khaironbiz@gmail.com>
2021-12-31 20:10:57 +0700

    menu-kecil

--- layout/menu-kecil/menu.php
+++ layout/menu-kecil/menu.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-info">
-  <a class="navbar-brand text-white" href="<?= $site_url ?>/soal/">Master Pengkajian</a>
+  <a class="navbar-brand text-white" href="<?= $site_url ?>/soal/">Master</a>
   <button class="navbar-toggler text-white" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
